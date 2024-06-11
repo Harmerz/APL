43c526c8601bbb5407e5d236fc3c1207f763f621
Haikal Hilmi <haikal6255hilmi@gmail.com>
2024-06-12 05:06:08 +0700

    feat(go): add cache

--- go-auth/client.go
+++ go-auth/client.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	pb "APL/go-auth/pb/inventory"
+	"github.com/patrickmn/go-cache"
 
 	"google.golang.org/grpc"
 )
@@ -25,43 +26,62 @@ func main() {
 	defer conn.Close()
 	client := pb.NewTweetServiceClient(conn)
 
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
+	// Initialize cache with a default expiration time of 10 minutes, and which purges expired items every 30 minutes
+	c := cache.New(10*time.Minute, 30*time.Minute)
 
+	// URL to fetch
 	url := "https://x.com/PaddleHQ/status/1800160489479025031"
 
-	var r *pb.TweetResponse
-	for i := 0; i < retryCount; i++ {
-		r, err = client.GetTweetComment(ctx, &pb.TweetRequest{Url: url})
-		if err == nil {
-			break
+	// Try to get from cache
+	if cachedResponse, found := c.Get(url); found {
+		r := cachedResponse.(*pb.TweetResponse)
+		log.Printf("Fetched from cache:")
+		printResponse(r)
+	} else {
+		// Contact the server and print out its response.
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		var r *pb.TweetResponse
+		for i := 0; i < retryCount; i++ {
+			r, err = client.GetTweetComment(ctx, &pb.TweetRequest{Url: url})
+			if err == nil {
+				break
+			}
+			if i < retryCount-1 {
+				log.Printf("Retrying due to error: %v", err)
+				time.Sleep(time.Duration(i+1) * time.Second) // Exponential backoff
+			} else {
+				log.Fatalf("could not get tweet comments: %v", err)
+			}
 		}
-		if i < retryCount-1 {
-			log.Printf("Retrying due to error: %v", err)
-			time.Sleep(time.Duration(i+1) * time.Second) // Exponential backoff
-		} else {
-			log.Fatalf("could not get tweet comments: %v", err)
+
+		if r != nil {
+			// Store the response in the cache
+			c.Set(url, r, cache.DefaultExpiration)
+
+			log.Printf("Fetched from gRPC:")
+			printResponse(r)
 		}
 	}
+}
 
-	if r != nil {
-		log.Printf("URL: %v", r.GetUrl())
-		log.Printf("Type: %v", r.GetType())
-		for _, comment := range r.GetData() {
-			user := comment.GetUser()
-			log.Printf("User Name: %v", user.GetName())
-			log.Printf("User Created At: %v", user.GetCreatedAt())
-			log.Printf("User Screen Name: %v", user.GetScreenName())
-			log.Printf("User Favourites Count: %v", user.GetFavouritesCount())
-			log.Printf("User Followers Count: %v", user.GetFollowersCount())
-			log.Printf("User Friends Count: %v", user.GetFriendsCount())
-			log.Printf("User URL: %v", user.GetUrl())
-			log.Printf("Views: %v", comment.GetViews())
-			log.Printf("Tweet: %v", comment.GetTweet())
-			log.Printf("Like: %v", comment.GetLike())
-			log.Printf("Created At: %v", comment.GetCreatedAt())
-			log.Printf("ID Comment: %v", comment.GetIdComment())
-		}
+func printResponse(r *pb.TweetResponse) {
+	log.Printf("URL: %v", r.GetUrl())
+	log.Printf("Type: %v", r.GetType())
+	for _, comment := range r.GetData() {
+		user := comment.GetUser()
+		log.Printf("User Name: %v", user.GetName())
+		log.Printf("User Created At: %v", user.GetCreatedAt())
+		log.Printf("User Screen Name: %v", user.GetScreenName())
+		log.Printf("User Favourites Count: %v", user.GetFavouritesCount())
+		log.Printf("User Followers Count: %v", user.GetFollowersCount())
+		log.Printf("User Friends Count: %v", user.GetFriendsCount())
+		log.Printf("User URL: %v", user.GetUrl())
+		log.Printf("Views: %v", comment.GetViews())
+		log.Printf("Tweet: %v", comment.GetTweet())
+		log.Printf("Like: %v", comment.GetLike())
+		log.Printf("Created At: %v", comment.GetCreatedAt())
+		log.Printf("ID Comment: %v", comment.GetIdComment())
 	}
 }
--- go-auth/go.mod
+++ go-auth/go.mod
@@ -5,6 +5,7 @@ go 1.21.6
 require google.golang.org/grpc v1.64.0
 
 require (
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
